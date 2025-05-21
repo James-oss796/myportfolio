bc68ee214c971e33f5ea66e31ca88d5923464125
james_brian <147589744+James-oss796@users.noreply.github.com>
2025-05-21 23:03:09 +0300

    Update Skills.jsx

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -19,10 +19,12 @@ export default function Skills() {
             className='md:w-1/2 md:pr-12 md:border-r md:border-b-0 mb-10 md:mb-0 pd-10 border-b border-orange-400'>
                 <h1 data-aos='fade-left' data-aos-delay='500' className='flex flex-col justify-center items-center  sm:text-4xl text-7xl font-extrabold title-font mb-6 text-white'>Skills</h1>
                 <p data-aos='fade-right' data-aos-delay='500' className='leading-relaxed text-base'>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi eum asperiores quam, maxime hic aspernatur,
-                     illo nam quasi obcaecati rem expedita sunt laboriosam dolorem non porro ab tenetur? Tempora, pariatur!
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, animi. Velit non neque distinctio ab
-                      perspiciatis quo id autem repellat esse earum, consectetur et rem quaerat veritatis fuga possimus nam.
+                    - DevOps Tools: Docker, Kubernetes, Jenkins, Git, Terraform, Ansible, Prometheus, Grafana
+- Cloud Platforms: AWS (EC2, S3, Lambda), Azure, Google Cloud Platform
+- Networking: TCP/IP, DNS, DHCP, VPN, Firewall Configuration, Routing Protocols (OSPF, BGP)
+- Scripting & Programming: Bash, Python, YAML
+- Monitoring & Logging: ELK Stack, Nagios, CloudWatch
+- Operating Systems: Linux (Ubuntu, CentOS), Windows Server
                 </p>
             </div>
             <div data-aos='fade-left' data-aos-delay='500' className='flex flex-col md:w-1/2 md:pl-12'>
